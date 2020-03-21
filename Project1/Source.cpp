2eee54682305e9c724659b89ebd1f0787643e10b
atherfawaz <atherfawaz@users.noreply.github.com>
2020-03-21 16:12:19 +0500

    Implemented Question 3. Working on Q4.

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <iostream>
 #include <time.h>
+#include <vector>
 
 namespace CustomerPriority {
 	/*
@@ -57,17 +58,38 @@ namespace ArtGallery {
 	that comes within d distance on his left and right.Design an algorithm for finding a placements 
 	of guards that uses the minimum number of guards to guard all the objects with positions in P.
 	*/
-	int GUARDDISTANCE = 2;		// d
+	int GUARDDISTANCE = 10;		// d
 	void findPlacement() {
-		for (int i = 0; i < 10; i++) {
 		int paintinglocations[10];
-			paintinglocations[i] = rand() % 10 + 1;
+		for (int i = 0; i < 10; i++) {
+			paintinglocations[i] = rand() % 50 + 1;
+		}
+		int n = sizeof(paintinglocations) / sizeof(paintinglocations[0]);
+		std::sort(paintinglocations, paintinglocations + n);
+		std::vector<int> positions;
+		positions.push_back(paintinglocations[0]);
+		int previous = 0;
+		int nextposition = 1;
+		for (int i = 1; i < 9; i++) {
+			if (positions[previous] + GUARDDISTANCE < paintinglocations[nextposition]) {
+				//we'll need another guard at the next painting
+				positions.push_back(paintinglocations[nextposition]);
+				previous++;
+				nextposition++;
+			}
+			nextposition++;
+		}
+		std::cout << "###QUESTION 3###\n";
+		std::cout << "Guards should be positioned at the following locations: ";
+		for (int i = 0; i < positions.size(); i++) {
+			std::cout << positions[i] << " ";
 		}
 	}
 }
 
 namespace PrintingFlexes {
-	/*You are planning to start a new business of printing flexes. In the first 
+	/*
+	You are planning to start a new business of printing flexes. In the first 
 	step you have bought a printing machine. Every day you receive printing orders 
 	from n different customers. Customer Ci’s job requires time ti. The wait time of 
 	a customer Ci is wi which is the amount of time the customer waited till its job 
@@ -76,8 +98,10 @@ namespace PrintingFlexes {
 	dependent on the wait time. The smaller the wait time the happier your customer is. 
 	Devise a schedule that minimizes the total wait time of all the n customers. Suppose 
 	there are three customer with job time t1=4, t2=5, t3=2. If you schedule (1, 2, 3) 
-	then w1=4, w2=9, w3=11 total wait time is 4+9+11=24*/
+	then w1=4, w2=9, w3=11 total wait time is 4+9+11=24
+	*/
 	int CUSTOMERCOUNT = 0;
+	int TOTALTIME;
 	class Customer {
 	private:
 		int position;
@@ -102,22 +126,66 @@ namespace PrintingFlexes {
 		Customer customers[10];
 		int n = sizeof(customers) / sizeof(customers[0]);
 		std::sort(customers, customers + n, compareWaitingTimes);
-		std::cout << "###QUESTION 6###\n";
+		std::cout << "\n###QUESTION 6###\n";
 		std::cout << "The schedule for printing flexes is as follows: ";
 		for (int i = 0; i < 10; i++) {
 			customers[i].print();
 		}
-		int totaltime = customers[0].getJobTime();
+		int TOTALTIME = customers[0].getJobTime();
 		for (int i = 1; i < 10; i++) {
-			totaltime += (totaltime + customers[i].getJobTime());
+			TOTALTIME += (TOTALTIME + customers[i].getJobTime());
 		}
-		std::cout << "\nThe total waiting for all customers is: " << totaltime << std::endl;
+		std::cout << "\nThe total waiting for all customers is: " << TOTALTIME << std::endl;
+	}
+}
+
+namespace Triathalon {
+	/*
+	Suppose you want to organize activities for some campers. This includes a triathalon 
+	exercise: each contestant must swim 20 laps of a pool, then bike 10 miles, then run 3 
+	miles. The contestants must use the pool one at a time. In other words, first one contestant 
+	swims the 20 laps, gets out, and starts biking. As soon as this first person is out of the 
+	pool, a second contestant begins swimming the 20 laps; as soon as he or she is out and starts 
+	biking, a third contestant begins swimming . . . and so on. Each contestant has a projected 
+	swimming time (the expected time it will take him or her to complete the 20 laps), a projected 
+	biking time (the expected time it will take him or her to complete the 10 miles of bicycling), 
+	and a projected running time (the time it will take him or her to complete the 3 miles of running). 
+	You want to decide on a schedule for the triathalon: an order in which to sequence the starts of 
+	the contestants. The completion time of a schedule is the earliest time at which all contestants 
+	will be finished with all three legs of the triathalon, assuming they each spend exactly their 
+	projected swimming, biking, and running times on the three parts. (Again, note that participants 
+	can bike and run simultaneously, but at most one person can be in the pool at any time.) 
+	What’s the best order for sending people out, if one wants the whole competition to be over 
+	as early as possible?
+	*/
+	int RUNNERCOUNTER = 0;
+	class Camper {
+	private:
+		int runningtime;
+		int bikingtime;
+		int swimmingtime;
+		int position;
+	public:
+		Camper() {
+			RUNNERCOUNTER++;
+			runningtime = rand() % 20 + 1;
+			bikingtime = rand() % 20 + 1;
+			swimmingtime = rand() % 20 + 1;
+			position = RUNNERCOUNTER;
+		}
+		void print() {
+
+		}
+	};
+	void makeSchedule() {
+		Camper campers[10];
 	}
 }
 
 int main() {
 	srand(time(NULL));
+	ArtGallery::findPlacement();			//Q3
+	Triathalon::makeSchedule();				//Q4
 	PrintingFlexes::makeSchedule();			//Q6
 	CustomerPriority::makeSchedule();		//Q7
-	ArtGallery::findPlacement();			//Q3
 }
